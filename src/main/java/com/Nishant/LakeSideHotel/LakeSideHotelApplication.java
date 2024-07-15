4d342cc04fe1e8e5d1184229a4f68be34413ee2d
Nishant <nishant.dhamija@borderfree.io-at-527579388740>
2024-07-16 00:13:19 +0530

    Removed Print

--- src/main/java/com/Nishant/LakeSideHotel/LakeSideHotelApplication.java
+++ src/main/java/com/Nishant/LakeSideHotel/LakeSideHotelApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LakeSideHotelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LakeSideHotelApplication.class, args);System.out.print("Hello");
+		SpringApplication.run(LakeSideHotelApplication.class, args);
 	}
 
 }
