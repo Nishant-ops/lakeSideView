3e69b3e19278a4bea18980742896cd7c24403dec
NishantEllenox <nishant@ellenox.com>
2024-07-17 23:55:57 +0530

    Added Room Model

--- src/main/java/com/Nishant/LakeSideHotel/model/Room.java
+++ src/main/java/com/Nishant/LakeSideHotel/model/Room.java
@@ -1,4 +1,33 @@
 package com.Nishant.LakeSideHotel.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.List;
+import java.util.UUID;
 
+/**
+ * @author Gaurav Yadav
+ */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked = false;
+    @Lob
+    private Blob photo;
+
+    @OneToMany(mappedBy="room", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<BookedRoom> bookings;
+
+
 }
